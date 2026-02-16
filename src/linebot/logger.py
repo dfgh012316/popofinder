992d2488f31e73cf7047b5a31dfad9827eb0fde6
jerrychuang <jerrychuang@jerrytech.me>
2026-02-16 12:31:00 +0800

    feat: remove log file handler

--- src/linebot/logger.py
+++ src/linebot/logger.py
@@ -10,7 +10,6 @@ logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s [%(levelname)s] %(message)s",
     handlers=[
-        logging.FileHandler("app.log", encoding="utf-8"),
         logging.StreamHandler(),
     ],
 )
